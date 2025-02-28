cf6713f49f9d48660d8c71b937fd6b639876d7c8
RikiEhack007 <202000197@my.apiu.edu>
2025-03-01 01:34:36 +0700

    Add HRMS logo on Welcome blade page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,9 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
         <!-- Styles -->
         <style>
             html, body {
@@ -64,29 +67,37 @@
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
+             
+        <div class="flex-center position-ref full-height"> 
             <div class="content">
+
+            <div class="mx-auto mb-5 text-center">
+                <!-- Custom Logo Box -->
+                <div class="d-inline-block position-relative" style="width: 220px; height: 90px; border: 6px solid #0D3E5F; border-radius: 20px; background-color: #fff;">
+                    <h1 style="color: #0D3E5F; margin: 0; font-size: 4rem;">HRMS</h1>
+
+                    <!-- SMRU / BHF on the bottom-right corner -->
+                        <span style="
+                            position: absolute;
+                            bottom: -10px;
+                            right: 10px;
+                            background-color: white;
+                            padding: 0 8px;
+                            font-weight: 600;
+                            color: #0D3E5F;
+                            font-size: 0.9rem;
+                            ">
+                            SMRU / BHF
+                        </span>
+                </div>
+            </div>  
                 <div class="title m-b-md">
                     HRMS Backend API
                 </div>
 
                 <div class="links">
                     <a href="{{ url('/api/documentation') }}">API Documentation</a>
-                    <a href="https://github.com/EhDohWah/hrms-backend-api.git">GitHub</a>
+                    <a href="https://github.com/EhDohWah/hrms-backend-api-v1.git">GitHub</a>
                 </div>
             </div>
         </div>
